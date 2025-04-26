f53ed1963de5a57c31d9ca22d298d79adae45149
YashavanthaN8057 <yme1061920@gmail.com>
2025-04-26 16:51:43 +0530

    leaning vault and monitoring ~ auto push

--- vars.tf
+++ vars.tf
@@ -5,7 +5,9 @@ variable "zone_id" {}
 variable "port_no" {}
 variable "port_no_of_ssh" {}
 
-variable "ami" {}
+variable "ami" {
+  default = "ami-0b2c655c71dbd69d9"
+}
 # variable "port_no_of_node_exporter" {}
 # variable "token" {}
 # variable "prometheus_server" {}
