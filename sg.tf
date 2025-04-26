06d5104231e4b8ea74da624b99ca139ffdcec823
YashavanthaN8057 <yme1061920@gmail.com>
2025-04-26 17:06:22 +0530

    leaning vault and monitoring ~ auto push

--- sg.tf
+++ sg.tf
@@ -26,12 +26,12 @@ resource "aws_security_group" "main" {
 #     cidr_blocks = var.prometheus_server
 #   }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "${var.name}-${var.env}-sg"
